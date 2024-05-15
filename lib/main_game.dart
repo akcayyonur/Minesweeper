823c684d8628b3c7d4d973934dbf674022528480
akcayyonur <167646813+akcayyonur@users.noreply.github.com>
2024-05-15 16:36:48 +0300

    Update main_game.dart
    Made changes on the appBar, added the restart button and settings button

--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum Image {
+enum Imagee {
   zero,
   one,
   two,
@@ -16,12 +16,45 @@ class mainGame extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("game page", style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.grey,
+        automaticallyImplyLeading: false, // To prevent a default back button
+        title: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: IconButton(
+                onPressed: _restartGame,
+                icon: Image.asset(
+                  "assets/smileyface.png",
+                  width: 50,
+                  height: 40,
+                ),
+                iconSize: 30,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                onPressed: _goToSettings,
+                icon: Icon(Icons.settings),
+                iconSize: 30,
+              ),
+            ),
+          ],
+        ),
+        toolbarHeight: 56.0,
       ),
       body: Center(
         child: Text("where game should be at"),
       ),
     );
+  }
+
+  void _restartGame() {
+    // Add your restart game logic here
+  }
+
+  void _goToSettings() {
+    // Action for the right button
   }
 }
